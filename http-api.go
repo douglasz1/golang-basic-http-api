b9a6e2241c4a5399d520b6a37d2d88715e205c82
douglasz1 <douglas.santana.dev@gmail.com>
2021-01-11 16:05:10 -0300

    adjusting variables

--- http-api.go
+++ http-api.go
@@ -7,16 +7,26 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var data []string = []string{}
+
 func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/test", test)
 
+	router.HandleFunc("/add/{data}", addItem)
+
 	http.ListenAndServe(":5000", router)
 }
 
 func test(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(struct {
 		ID string
 	}{ID: "555"})
+}
+
+fun addItem(w http.ResponseWriter, r *http.Request) {
+	routeVariable := mux.Vars(r)["data"]
+	data = append(data, routeVariable)
 }
