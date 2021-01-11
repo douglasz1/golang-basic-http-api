18c7349916d871c6db4c2f12551e5f9733e0e4a9
douglasz1 <douglas.santana.dev@gmail.com>
2021-01-11 16:44:54 -0300

    :fire:

--- http-api.go
+++ http-api.go
@@ -7,28 +7,27 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var data []string = []string{}
+// Item is a struct that groups all necessary fields into a single unit
+type Item struct {
+	data string
+}
+
+var data []Item = []Item{}
 
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/test", test)
-
-	router.HandleFunc("/add/{item}", addItem).Methods("POST")
+	router.HandleFunc("/add", addItem).Methods("POST")
 
 	http.ListenAndServe(":5000", router)
 }
 
-func test(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(struct {
-		ID string
-	}{ID: "555"})
-}
-
 func addItem(w http.ResponseWriter, r *http.Request) {
-	routeVariable := mux.Vars(r)["item"]
-	data = append(data, routeVariable)
+	// get Item value from the JSON body
+	var newItem Item
+	json.NewDecoder(r.Body).Decode(&newItem)
+
+	data = append(data, newItem)
 
 	w.Header().Set("Content-Type", "application/json")
 
