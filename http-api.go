680e0184e343e5f524033361bab80a6f1cd40323
douglasz1 <douglas.santana.dev@gmail.com>
2021-01-11 15:43:01 -0300

    fixing stuffs

--- http-api.go
+++ http-api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,8 +11,12 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/test", test)
+
+	http.ListenAndServe(":5000", router)
 }
 
 func test(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is a test")
+	json.NewEncoder(w).Encode(struct {
+		ID string
+	}{ID: "555"})
 }
